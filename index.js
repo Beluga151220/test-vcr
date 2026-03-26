f8836d354c75b28067e2a87842c72e9cd4c113bf
Long Nguyen <longnn7@vng.com.vn>
2026-03-26 14:01:48 +0700

    Cập nhật giao diện Soft UI đẹp hơn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+// Cấu hình để NodeJS hiểu các file tĩnh (hình ảnh, css)
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
-  res.send('<h1>Chào Longnn7! Website đã chạy thành công qua Docker và VCR.</h1>');
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Web đang chạy tại http://localhost:${port}`);
+  console.log(`Web đẹp đang chạy tại http://localhost:${port}`);
 });
